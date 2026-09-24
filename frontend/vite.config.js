0ea7f9b407e22816196fb97b0234c964f2226877
sandipbaste <sandipbaste999@gmail.com>
2026-09-24 22:49:08 +0530

    s commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',   // ✅ हे महत्त्वाचे आहे!
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 10000,   // <-- Render 10000 पोर्ट वापरतो
+    port: process.env.PORT || 10000,
     strictPort: false,
     allowedHosts: [
       'jeevan-samruddhi-spirulina.onrender.com',
